acd5b98a35822c216519f92d12f6de8e5535615e
felix <felixmeissner@me.com>
2016-04-18 17:13:38 +0200

    mein_erstes_commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,5 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outlet1: UIButton!
+    @IBOutlet weak var TextFeld: UITextField!
+    @IBOutlet weak var Outlet2: UIButton!
+    @IBOutlet weak var Label1: UILabel!
+    
+    @IBAction func Action1(sender: AnyObject) {
+        if let name = TextFeld.text where !name.isEmpty {
+            Label1.text = "Hello, \(name)"
+        } else {
+            Label1.text = "Hello, World!"
+        }
+    }
 }
 
